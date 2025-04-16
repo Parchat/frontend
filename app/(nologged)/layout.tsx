93618a25a21a792cafb766195aaa3b81fd16aeb1
DanilsGit <danielestebanmarquez12@gmail.com>
2025-04-16 16:40:28 -0500

    add metadata

--- app/(nologged)/layout.tsx
+++ app/(nologged)/layout.tsx
@@ -2,6 +2,24 @@ import '@/app/_ui/globals.css';
 import { raleway } from '../_ui/fonts';
 import Header from '../_components/Header';
 import Footer from '../_components/Footer';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Parchat',
+  description:
+    'Comunícate y conecta con diferentes comunidades a través de salas de chat en tiempo real',
+  icons: {
+    icon: '/favicon.jpg',
+  },
+  openGraph: {
+    title: 'Parchat - Salas de chat para comunidades',
+    description:
+      'Únete a diferentes comunidades y chatea en tiempo real. Crea tus propias salas y conecta con personas que comparten tus intereses.',
+    images: ['/favicon.jpg'], // Ruta de la imagen para compartir
+    type: 'website',
+    locale: 'es_ES',
+  },
+};
 
 export default function RootLayout({
   children,
